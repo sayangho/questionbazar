1140b8b3710a71537786237a4e64561e30fe88f9
Azad Singh <71605755+azadhash@users.noreply.github.com>
2021-09-12 01:15:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile-menu')
+const menulinks = document.querySelector('.navbar_menu')
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('is-actuive');
+    menulinks.classList.toggle('active');
+})
